292fe79769308c989067b1fd43950e195f99f191
natsuki <natsukis6.ayase@gmail.com>
2022-12-06 11:19:23 +0900

    変更テスト

--- welcome.blade.php
+++ welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-//コミット用
+//コミット用です
 
     </head>
     <body class="antialiased">
